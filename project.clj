9a15b7efa65453db00d9ff1a51714f7fc5f87c5e
Alistair Dutton <alistair.dutton@kaluza.com>
2020-12-04 13:25:28 +0000

    Bump to v2.0.2

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject ovotech/duct.middleware.ring-jwt "2.0.0"
+(defproject ovotech/duct.middleware.ring-jwt "2.0.2"
   :description "Integrant keys for ovotech/ring-jwt."
   :url "http://github.com/ovotech/duct.middleware.jwt"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[ovotech/ring-jwt "2.0.0"]]
+  :dependencies [[ovotech/ring-jwt "2.0.2"]]
   :profiles {:provided {:dependencies [[integrant "0.8.0"]
                                        [duct/core "0.8.0"]]}})
