b662814c2ca602553b0c6521d40b4515c7ca19c9
Alistair Dutton <alistair.dutton@kaluza.com>
2024-01-25 17:11:41 +0000

    Bump version to 2.7.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject net.clojars.kelveden/duct.middleware.ring-jwt "2.6.0"
+(defproject net.clojars.kelveden/duct.middleware.ring-jwt "2.7.0"
   :description "Integrant keys for kelveden/ring-jwt."
   :url "http://github.com/kelveden/duct.middleware.jwt"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[net.clojars.kelveden/ring-jwt "2.6.0"]]
+  :dependencies [[net.clojars.kelveden/ring-jwt "2.7.0"]]
   :profiles {:provided {:dependencies [[integrant "0.8.0"]
                                        [duct/core "0.8.0"]]}})
